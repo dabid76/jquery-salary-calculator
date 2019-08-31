441972502134a9cca429ef4cc7c62eca57a95f74
David Diaz <daemdisa@aol.com>
2019-08-31 11:20:53 -0500

    yay got the table

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 $(document).ready( readyNow );
 
-let employee = [
-    // name: "FirstName",
-    // last: "LastName",
-    // id: "ID",
-    // title: "Title",
-    // annul: "AnnuSalalary"
-];
+let employee = [];
 
 function readyNow() {
     listOfEmployee();
@@ -21,11 +15,11 @@ function readyNow() {
                     <th>Title</th>
                     <th>Annual Salary</th>
                 </tr>
-            </thead>`
+            </thead>
+            <tbody>`
             ) // end let
     $( 'table' ).append( data );
 
-    
 } // end function
 
 function handleSubmit(){
@@ -34,6 +28,33 @@ function handleSubmit(){
    let idVal = $( '#id' ).val();
    let titleVal = $( '#title' ).val();
    let annuVal = $( '#annual' ).val();
+
+   if( nameVal === '' ){
+    alert("Please fill out the First Name input");
+    $('#firstName').addClass('error');
+
+} else if( lastVal === ''){
+    alert("Please fill out the Last Name input");
+    $('#lastName').addClass('error');
+
+} else if( idVal === ''){
+    alert("Please fill out the ID input");
+    $('#id').addClass('error');
+
+} else if( titleVal === ''){
+    alert("Please fill out the Title input");
+    $('#title').addClass('error');
+
+} else if( annuVal === ''){
+    alert("Please fill out the Annual Salary input");
+    $('#annual').addClass('error');
+
+} else {
+    $('#firstName').removeClass('error');
+    $('#lastName').removeClass('error');
+    $('#id').removeClass('error');
+    $('#title').removeClass('error');
+    $('#annual').removeClass('error');
 
     let newInfo = {
         name: nameVal,
@@ -54,28 +75,24 @@ function handleSubmit(){
     $( '#annual' ).val('');
 
     console.log( newInfo );
-    
+} // end else statement
 } // end function
 
 function listOfEmployee(){
-    $('#listEmployee').empty();
+    $('tbody').empty();
     console.log(employee);
     
-    // for (let i = 0; i < employee.length; i++) {
     for( person of employee ){
-        let $tbody = $(`
-        <tbody>
+        data = $(`
             <tr>
                 <td>${person.name}</td>
                 <td>${person.last}</td>
                 <td>${person.id}</td>
                 <td>${person.title}</td>
                 <td>${person.annul}</td>
-            </tr>
-        </tbody>
-        </table>`
+            </tr><button class="deleteEmployee"></button>
+            </table>`
             ) // end let
-        $tbody.data( 'info', employee );
-        $( 'table' ).append( $tbody );
+        $( 'tbody' ).append( data );
     } // end for loop
 } // end function
